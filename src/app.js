8136e8f68023d9eb818836a5ee6d1475cae1ee64
Deepak Guptha <vzan2012@gmail.com>
2019-09-26 17:01:51 +0530

    feat(user): Updation of the scripts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
-const greeting = `Hello Delta Star`;
-console.log(greeting);
+import { person, sayHello } from './lib';
 
-const greetname = (name) => console.log(`Welcome: ${name}`);
 
-greetname('vzan2012');
+// const greeting = `Hello Delta Star`;
+// console.log(greeting);
+
+// const greetname = (name) => console.log(`Welcome: ${name}`);
+// greetname('vzan2012');
+
+// console.log(sayHello(person.name));
+
+async function getPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    console.log(response);
+    return response;
+};
+
+getPosts().then((response) => response.json()).then((json) => console.log(json));
---
+++ src/lib.js
@@ -0,0 +1,6 @@
+export const person = {
+    name: 'John Doe',
+    location: 'Earth'
+}
+
+export const sayHello = (name) => console.log(`Hello ${name}`);
