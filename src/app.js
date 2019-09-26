420570fb911b30011256c3c0b48ce42cdfb760be
Deepak Guptha <vzan2012@gmail.com>
2019-09-26 12:18:32 +0530

    chore(config): Updation of package.json and webpack config files

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const greeting = `Hello Delta Star`;
+console.log(greeting);
+
+const greetname = (name) => console.log(`Welcome: ${name}`);
+
+greetname('vzan2012');
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-transform-runtime"]
+        }
+      }
+    ]
+  }
+};
